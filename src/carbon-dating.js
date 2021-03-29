47d8ba068c5fa6f4f8c7e053001db9214e7a06fe
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2021-03-29 23:08:21 +0300

    feat: carbon-dating.js task resolved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY= 15;  // activity from the isotope’s identity
+const HALF_LIFE_PERIOD= 5730;  // half life period t 1/2
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  if(isNaN(sampleActivity)
+      || typeof  sampleActivity !== 'string'
+      || arguments.length == 0
+      || sampleActivity <= 0
+      || sampleActivity > 15) return false;
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k) // t=ln(N0/N)/k
+  return t;
 };
