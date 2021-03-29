c87932570df1df7f116e626c00ca3cf2cf22f259
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2021-03-29 21:50:29 +0300

    feat: add correctly works with an empty array && throws an Error if arr is not an Array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+
+  if(!arr.length){
+    return []
+  }
 };
